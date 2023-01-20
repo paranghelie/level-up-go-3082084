5703d8d822e496dba9f928c990c5a408471a71ac
Adrian Dinca <dinca.adrian@gmail.com>
2023-01-20 09:16:54 +0000

    First ex

--- 01_01b/main.go
+++ 01_01b/main.go
@@ -10,12 +10,19 @@ var expectedFormat = "2006-01-02"
 
 // parseTime validates and parses a given date string.
 func parseTime(target string) time.Time {
-	panic("NOT IMPLEMENTED")
+	convertedTime, err := time.Parse(expectedFormat, target)
+	if err != nil || time.Now().After(convertedTime) {
+		log.Fatal("Error at parsing time")
+	}
+
+	return convertedTime
 }
 
 // calcSleeps returns the number of sleeps until the target.
 func calcSleeps(target time.Time) float64 {
-	panic("NOT IMPLEMENTED")
+	remainingHours := time.Until(target).Hours()
+
+	return remainingHours / 24
 }
 
 func main() {
